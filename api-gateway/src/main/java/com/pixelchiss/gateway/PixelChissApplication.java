026bb7122587c1a32ae2f1cddf0410c2d709e8b5
Mateo-Castro06 <mateoruedacastro11@gmail.com>
2026-09-15 17:42:00 -0500

    Initial API Gateway

---
+++ api-gateway/src/main/java/com/pixelchiss/gateway/PixelChissApplication.java
@@ -0,0 +1,13 @@
+package com.pixelchiss.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelChissApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelChissApplication.class, args);
+    }
+
+}
